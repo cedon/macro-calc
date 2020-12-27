6c1152b45074bbc593dcbd178bbf1641b03ce611
Jon Liebold <jliebold@outlook.com>
2020-12-27 09:59:50 -0500

    Created Results component as child to Calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,36 @@
 <template>
-  <form @submit.prevent="onSubmit">
-    <label for="sex">Sex:</label>
-    <select id="sex" name="sex">
-      <option value="male">Male</option>
-      <option value="female">Female</option>
-    </select>
-    <label for="weight">Weight:</label>
-    <input id="weight" type="number" name="weight" required="required"/>
-    <label for="weightUnits">Units:</label>
-    <select id="weightUnits" name="weightUnits">
-      <option value="pounds">Pounds</option>
-      <option valus="kilograms">Kilograms</option>
-    </select>
-    <label for="bodyFat">Body Fat %:</label>
-    <input id="bodyFat" type="number" name="bodyFat" required="required"/>
-  </form>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <label for="sex">Sex:</label>
+      <select id="sex" name="sex">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+      <label for="weight">Weight:</label>
+      <input id="weight" type="number" name="weight" required="required"/>
+      <label for="weightUnits">Units:</label>
+      <select id="weightUnits" name="weightUnits">
+        <option value="pounds">Pounds</option>
+        <option valus="kilograms">Kilograms</option>
+      </select>
+      <label for="bodyFat">Body Fat %:</label>
+      <input id="bodyFat" type="number" name="bodyFat" required="required"/>
+      <button>Submit</button>
+    </form>
+    <section>
+      <Results />
+    </section>
+  </div>
 </template>
 
 <script>
+import Results from '@/components/Results'
+
 export default {
   name: 'Calculator',
+  components: {
+    Results
+  },
   data () {
     return {
       calc: {}
---
+++ src/components/Results.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Results Go Here</p>
+</template>
+
+<script>
+export default {
+  name: 'Results'
+}
+</script>
+
+<style scoped>
+
+</style>
